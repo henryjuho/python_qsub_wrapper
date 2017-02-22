c991a535d09fe6f1cd17c7514ec4da872524af54
henryjuho <juhob@hotmail.co.uk>
2017-02-22 19:05:52 +0000

    Added intial sbatch comp, hold jobs not implemented

--- qsub.py
+++ qsub.py
@@ -17,10 +17,8 @@ def float2qsubtime(time_float):
 def q_script(cmd, out, mo='NONE', t=8.0, rmem=2, mem=6, hold='NONE',
              jid='DEFAULT', tr=1, evolgen=False, node='0', array='no_array'):
 
-    # todo modify this for sbatch or write sbatch function?
-
     """
-    function that prints a bash script suitable for submission to the son of grid engine, using qsub
+    function that returns the contents a bash script suitable for submission to the son of grid engine, using qsub
 
     :param cmd: list
     :param out: str
@@ -109,11 +107,11 @@ def q_script(cmd, out, mo='NONE', t=8.0, rmem=2, mem=6, hold='NONE',
     return shell_contents, output_name
 
 
-def q_print(cmd, out, mo='NONE', t=8.0, rmem=2, mem=6, hold='NONE', jid='DEFAULT', tr=1, evolgen=False,
-            node='0', array='no_array'):
+def s_script(cmd, out, mo='NONE', t=8.0, rmem=2, mem=6, hold='NONE',
+             jid='DEFAULT', tr=1, evolgen=False, node='0', array='no_array'):
 
     """
-    function that prints a bash script suitable for submission to the son of grid engine, using qsub
+    function that returns the contents of a bash script suitable for submission to slurm, using sbatch
 
     :param cmd: list
     :param out: str
@@ -130,17 +128,83 @@ def q_print(cmd, out, mo='NONE', t=8.0, rmem=2, mem=6, hold='NONE', jid='DEFAULT
     :return:
     """
 
-    script = q_script(cmd, out, mo=mo, t=t, rmem=rmem, mem=mem, hold=hold,
-                      jid=jid, tr=tr, evolgen=evolgen, node=node, array=array)[0]
+    # check options
+    if type(cmd) is not list:
+        raise TypeError('cmd must be a list')
 
-    print(script)
+    if type(cmd[0]) is not str or type(node) is not str:
+        raise TypeError('entries in cmd list and node must be str')
+
+    if not mo == 'NONE':
+        if type(mo) is not list:
+            raise TypeError('mo must be a list')
+        for m in mo:
+            if m not in modules.keys():
+                raise KeyError(m + ' not a valid module')
+
+    if not hold == 'NONE':
+        if type(hold) is not list:
+            raise TypeError('hold must be a list')
+
+    if type(t) is int:
+        t = float(t)
+
+    if type(t) is not float:
+        raise TypeError('t must be a float')
+
+    if type(rmem) is not int or type(mem) is not int \
+            or type(tr) is not int:
+
+        raise TypeError('rmem, mem, tr and node must be integers')
+
+    if array != 'no_array' and type(array) is not list:
+        raise TypeError('array must be a list')
+
+    # set variables
+    run_time = '#SBATCH -t ' + float2qsubtime(t) + '\n'
+    memory = '#SBATCH --mem_per_cpu ' + str(rmem) + 'G\n'
+    file_pos = out.rfind('/')+1  # identifies position of file name in path string
+    if jid == 'DEFAULT':
+        output_name = out[0:file_pos] + out[file_pos:] + '_job.sh'
+    else:
+        output_name = out[0:file_pos] + jid
+    outs = ('\n#SBATCH -J ' + output_name[output_name.rfind('/')+1:] +
+            '\n#SBATCH -o '+out+'.out'
+            '\n#SBATCH -e '+out+'.error\n')
+    out_dir_path = out[:out.rfind('/') + 1]
+    if not os.path.isdir(out_dir_path):
+        os.makedirs(out_dir_path)
+    node_str = ''
+    if node != '0':
+        node_str = '#SBATCH -w ' + node + '\n'
+
+    # construct shell contents
+    shell_contents = '#!/bin/bash\n\nsource ~/.bash_profile\n\n'
+    if not mo == 'NONE':
+        for m in mo:
+            shell_contents += 'module load  ' + m + '\n'
+    if array != 'no_array':
+        shell_contents += '\n#SBATCH -a ' + str(array[0]) + '-' + str(array[1]) + '\n'
+    shell_contents += run_time + memory + '\n'
+    if tr != 1:
+        shell_contents += '#SBATCH -n ' + str(tr) + '\n'
+    shell_contents += node_str
+    shell_contents += outs + '\n'
+    # if hold is not 'NONE':
+    #     hold = ','.join(hold)
+    #     shell_contents += '#$-hold_jid ' + hold + '\n\n'
+    for x in cmd:
+        shell_contents += x + '\n'
+
+    # output shell script string
+    return shell_contents, output_name
 
 
-def q_write(cmd, out, mo='NONE', t=8.0, rmem=2, mem=6, hold='NONE', jid='DEFAULT', tr=1, evolgen=False,
-            node='0', array='no_array'):
+def q_print(cmd, out, mo='NONE', t=8.0, rmem=2, mem=6, hold='NONE', jid='DEFAULT', tr=1, evolgen=False,
+            node='0', array='no_array', scheduler='SGE'):
 
     """
-    function that writes a bash script suitable for submission to the son of grid engine, using qsub
+    function that prints a bash script suitable for submission to the son of grid engine or slurm, using qsub or sbatch
 
     :param cmd: list
     :param out: str
@@ -152,13 +216,57 @@ def q_write(cmd, out, mo='NONE', t=8.0, rmem=2, mem=6, hold='NONE', jid='DEFAULT
     :param jid: str
     :param tr: int
     :param evolgen: bool
-    :param node: int
+    :param node: str
     :param array: list
+    :param scheduler: str
     :return:
     """
 
-    script_data = q_script(cmd, out, mo=mo, t=t, rmem=rmem, mem=mem, hold=hold,
-                           jid=jid, tr=tr, evolgen=evolgen, node=node, array=array)
+    sched_opts = {'SGE', 'SLURM'}
+    if scheduler not in sched_opts:
+        raise ValueError('Only SGE and SLURM schedulers are supported at present!')
+
+    if scheduler == 'SGE':
+        script = q_script(cmd, out, mo=mo, t=t, rmem=rmem, mem=mem, hold=hold,
+                          jid=jid, tr=tr, evolgen=evolgen, node=node, array=array)[0]
+    else:
+        script = s_script(cmd, out, mo=mo, t=t, rmem=rmem, mem=mem, hold=hold,
+                          jid=jid, tr=tr, evolgen=evolgen, node=node, array=array)[0]
+    print(script)
+
+
+def q_write(cmd, out, mo='NONE', t=8.0, rmem=2, mem=6, hold='NONE', jid='DEFAULT', tr=1, evolgen=False,
+            node='0', array='no_array', scheduler='SGE'):
+
+    """
+    function that prints a bash script suitable for submission to the son of grid engine or slurm, using qsub or sbatch
+
+    :param cmd: list
+    :param out: str
+    :param mo: list
+    :param t: float
+    :param rmem: int
+    :param mem: int
+    :param hold: list
+    :param jid: str
+    :param tr: int
+    :param evolgen: bool
+    :param node: str
+    :param array: list
+    :param scheduler: str
+    :return:
+    """
+
+    sched_opts = {'SGE', 'SLURM'}
+    if scheduler not in sched_opts:
+        raise ValueError('Only SGE and SLURM schedulers are supported at present!')
+
+    if scheduler == 'SGE':
+        script_data = q_script(cmd, out, mo=mo, t=t, rmem=rmem, mem=mem, hold=hold,
+                               jid=jid, tr=tr, evolgen=evolgen, node=node, array=array)
+    else:
+        script_data = s_script(cmd, out, mo=mo, t=t, rmem=rmem, mem=mem, hold=hold,
+                               jid=jid, tr=tr, evolgen=evolgen, node=node, array=array)
 
     script = script_data[0]
     output_name = script_data[1]
@@ -169,11 +277,11 @@ def q_write(cmd, out, mo='NONE', t=8.0, rmem=2, mem=6, hold='NONE', jid='DEFAULT
     output.close()
 
 
-def q_sub(cmd, out, mo='NONE', t=8, rmem=2, mem=6, hold='NONE', jid='DEFAULT', tr=1, evolgen=False,
-          node='0', array='no_array'):
+def q_sub(cmd, out, mo='NONE', t=8.0, rmem=2, mem=6, hold='NONE', jid='DEFAULT', tr=1, evolgen=False,
+          node='0', array='no_array', scheduler='SGE'):
 
     """
-    function that writes and submits a bash script to the son of grid engine, using qsub
+    function that prints a bash script suitable for submission to the son of grid engine or slurm, using qsub or sbatch
 
     :param cmd: list
     :param out: str
@@ -187,11 +295,22 @@ def q_sub(cmd, out, mo='NONE', t=8, rmem=2, mem=6, hold='NONE', jid='DEFAULT', t
     :param evolgen: bool
     :param node: str
     :param array: list
+    :param scheduler: str
     :return:
     """
 
-    script_data = q_script(cmd, out, mo=mo, t=t, rmem=rmem, mem=mem, hold=hold,
-                           jid=jid, tr=tr, evolgen=evolgen, node=node, array=array)
+    sched_opts = {'SGE', 'SLURM'}
+    if scheduler not in sched_opts:
+        raise ValueError('Only SGE and SLURM schedulers are supported at present!')
+
+    if scheduler == 'SGE':
+        script_data = q_script(cmd, out, mo=mo, t=t, rmem=rmem, mem=mem, hold=hold,
+                               jid=jid, tr=tr, evolgen=evolgen, node=node, array=array)
+        sub_cmd = 'qsub '
+    else:
+        script_data = s_script(cmd, out, mo=mo, t=t, rmem=rmem, mem=mem, hold=hold,
+                               jid=jid, tr=tr, evolgen=evolgen, node=node, array=array)
+        sub_cmd = 'sbatch '
 
     script = script_data[0]
     output_name = script_data[1]
@@ -202,4 +321,4 @@ def q_sub(cmd, out, mo='NONE', t=8, rmem=2, mem=6, hold='NONE', jid='DEFAULT', t
     output.close()
 
     # submit script
-    subprocess.call('qsub ' + output_name, shell=True)
+    subprocess.call(sub_cmd + output_name, shell=True)
